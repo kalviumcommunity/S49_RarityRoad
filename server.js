30e363c3fe3dedc9abc973587e921f7bc5937c1b
yuvraj-05828 <yuvraj05828@gmail.com>
2024-02-16 15:00:40 +0530

    added node modules to gitignore

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PUBLIC_PORT || 3000;
 // define the ping route with the response in JSON
 app.get('/ping', (req,res)=>{
   res.json({
-    message: 'pong'
+    "message": 'pong'
   });
 });
 
