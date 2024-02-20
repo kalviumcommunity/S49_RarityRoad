eb55ca33d91afec284d803488c7c38bee792a64b
yuvraj-05828 <yuvraj05828@gmail.com>
2024-02-20 17:23:36 +0530

    updated server.js and connected mongodb compass

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const app = express();
-const port = process.env.PUBLIC_PORT || 3000;
+const port = 3000;
 
-// define the ping route with the response in JSON
-app.get('/ping', (req,res)=>{
-  res.json({
-    "message": 'pong'
-  });
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+app.use(express.json());
+
+// MongoDB connection URL
+const uri = process.env.mongoURi;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", async (req, res) => {
+  try {
+    // Connect to the MongoDB database
+    await client.connect();
+
+    // Check if the connection is successful
+    if (client.topology.isConnected()) {
+      res.json({ message: "pong", database_status: "Connected" });
+      console.log("yes");
+    } else {
+      res.json({ message: "pong", database_status: "Disconnected" });
+      console.log("no");
+    }
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// define the ping route
+app.get('/ping',(req,res)=>{
+  res.send('pong');
 });
 
 if (require.main === module) {
@@ -14,6 +43,5 @@ if (require.main === module) {
     console.log(`🚀 server running on PORT: ${port}`);
   });
 }
-
 
 module.exports = app;
