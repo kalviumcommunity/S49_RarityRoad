a152d6d9be9d7d549b06ea3f25a9758e044272fe
yuvraj-05828 <yuvraj05828@gmail.com>
2024-02-28 17:37:34 +0530

    connected frontend and backend

--- Frontend/cli/src/App.jsx
+++ Frontend/cli/src/App.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import Data from "./components/data"
+import axios from "axios"
+import { useState, useEffect } from "react";
 function App() {
+  const [data, setData] = useState();
+  useEffect(()=>{
+    axios.get('http://localhost:3005/carsname')
+    .then(res => {setData(res.data)})
+    .catch((err)=>console.log("Error in fetching data"))
+    },[])
+  
   return (
-    <div>
-    <h1>Product Information</h1>
-    <Data/>
-  </div>
+    <>
+      <div>{data && data.map((item)=>{
+        return(
+          <div key={item._id}>
+            <h2 >{item.Company}</h2> <hr />
+            <h2>{item.model}</h2>
+            <h2>{item.year}</h2>
+          </div>
+        )
+      })}</div>
+    </>
+
   )
 }
 
